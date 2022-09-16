f10d9adbb1de20f7e0c4018e80b3f2eba0202546
Raheel Ahmad <raheelA0@pm.me>
2022-09-16 17:55:42 -0400

    create template for a to-do list item

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import './styles/main.css'
 import 'normalize.css';
 import notepad from './assets/notepad.png'
 import daisy from './assets/daisy.png'
+import project from './assets/note-book.png'
+import check from './assets/checkmark.png'
 
 const appLogo = document.getElementById('appLogo');
 appLogo.src = notepad;
+
+const addProject = document.getElementById('addProject');
+addProject.src = project;
+
+const markComplete = document.getElementById('check');
+markComplete.src = check;
 
 const footerLogo = document.getElementById('footerLogo');
 footerLogo.src = daisy;
