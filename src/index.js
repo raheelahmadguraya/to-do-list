8f73bc9a753b4dd377f371ce224f31145e8fc607
Raheel Ahmad <raheelA0@pm.me>
2022-09-19 11:29:33 -0400

    complete checklist and description templates

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import 'normalize.css';
 import notepad from './assets/notepad.png'
 import daisy from './assets/daisy.png'
 import project from './assets/note-book.png'
-import check from './assets/checkmark.png'
+import check from './assets/success.png'
+import pencil from './assets/pencil.png'
+import deleteItem from './assets/bin.png'
+import addTask from './assets/plus.png'
 
 const appLogo = document.getElementById('appLogo');
 appLogo.src = notepad;
@@ -13,6 +16,15 @@ addProject.src = project;
 
 const markComplete = document.getElementById('check');
 markComplete.src = check;
+
+const edit = document.getElementById('editItem');
+edit.src = pencil;
+
+const deleteTask = document.getElementById('deleteItem');
+deleteTask.src = deleteItem;
+
+const addCheckListItem = document.getElementById('addItem');
+addCheckListItem.src = addTask;
 
 const footerLogo = document.getElementById('footerLogo');
 footerLogo.src = daisy;
