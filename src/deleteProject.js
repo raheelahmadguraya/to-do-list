a19a1097dd540077d3be6b04a1ded78ecc4b43ba
Raheel Ahmad <raheelA0@pm.me>
2022-09-21 17:02:18 -0400

    open modal if all projects have been deleted or if no project was ever created

--- src/deleteProject.js
+++ src/deleteProject.js
@@ -6,12 +6,20 @@ let deleteProjBtn = document.getElementById('deleteProject-button');
 
 const deleteProjectListener = () => {
   deleteProjBtn.addEventListener('click', event =>{
-    let currentProject = document.getElementsByClassName('currentProject')[0];
-    localStorage.removeItem(currentProject.id);
-    currentProject.remove();
-    let key = localStorage.key(0);
-    let newCurrentProject = JSON.parse(localStorage.getItem(key));
-    selectProject(newCurrentProject);
+    if (document.querySelector('.currentProject') != null) {
+      let currentProject = document.getElementsByClassName('currentProject')[0];
+      localStorage.removeItem(currentProject.id);
+      currentProject.remove();
+      if (localStorage.key(0) != null) {
+        let key = localStorage.key(0);
+        let newCurrentProject = JSON.parse(localStorage.getItem(key));
+        selectProject(newCurrentProject);
+      } else {
+        openModal();
+      }
+    } else {
+      openModal();
+    }
   })
 };
 
