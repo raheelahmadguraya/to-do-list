17ebfa1bb35600b87d4bdbd26d72958aa5d688fe
Raheel Ahmad <raheelA0@pm.me>
2022-09-24 01:44:23 -0400

    refactor code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import './styles/main.css'
 import 'normalize.css';
-import { setTodoListener } from './modules/showTodoModal';
-import { setAddProjectListener } from './modules/showProjectModal';
-import { setToDoSubmitListener } from './modules/collectToDoData';
-import { setProjectSubmitListener } from './modules/collectProjectData';
-import { deleteProjectListener } from './modules/deleteProject'
-import { setEditProjectListener } from './modules/editProjectModal'
-import { setEditProjectSubmitListener } from './modules/editProject'
+
 import { loadImages } from './modules/loadImages';
 import { load } from './modules/load';
 
